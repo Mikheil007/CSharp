8fa726299426effd96ca3e359467d45a93c95ee9
Mikheil007 <111079694+Mikheil007@users.noreply.github.com>
2022-09-08 23:05:03 +0300

    add Lesson1

---
+++ LESSON MASSIVE/Program.cs
@@ -0,0 +1,36 @@
+﻿int[,] CreateRandom2dArray()
+{
+    Console.Write("Input numbers of rows: ");
+    int rows = Convert.ToInt32(ConsoleReadLine());
+    Console.Write("Input numbers of columns: ");
+    int rows = Convert.ToInt32(ConsoleReadLine());
+    Console.Write("Input numbers of value: ");
+    int rows = Convert.ToInt32(ConsoleReadLine());
+    Console.Write("Input numbers of value: ");
+    int rows = Convert.ToInt32(ConsoleReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    
+        for(int j = 0; j < columns; j++)
+        newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+return newArray;
+
+
+void Show2dArray(int[,] array);
+}
+    for(int i = 0; i < array.GetLenght(0); j++)
+{
+    for(int i = 0; i < array.GetLenght(1); j++)
+    Console.Write(array[i,j] + " ");
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+{
+
+int[,] myArray = CreateRandom2dArray();
+}
+Show2dArray(myArray);
---
+++ Lesson1/Program.cs
@@ -0,0 +1,23 @@
+﻿int numbA;
+Console.WriteLine("numbA ");
+numbA = Convert.ToInt32(Console.ReadLine());
+int numbB;
+Console.WriteLine("numbB ");
+numbB = Convert.ToInt32(Console.ReadLine());
+int numbC;
+Console.WriteLine("numbC ");
+numbC = Convert.ToInt32(Console.ReadLine());
+int max;
+if (numbA > numbB)
+{
+max = numbA;
+}
+else
+{
+max = numbB;
+}
+if (numbC > max)
+{
+max = numbC;
+}
+Console.WriteLine($"максимальное число = {max}");
