929fcb182ed58e54cc89370d835b02725fb65945
Mikheil007 <111079694+Mikheil007@users.noreply.github.com>
2022-09-12 01:17:43 +0300

    сделал несколько задач самостоятельно

---
+++ LESSON 120922/Program.cs
@@ -0,0 +1,52 @@
+﻿// Console.WriteLine("Введите трёхзначное число ");
+// int numbA = Convert.ToInt32(Console.ReadLine());
+// if (numbA > 99 && numbA < 1000)
+// {
+// int numbB = numbA % 100 / 10;
+// Console.WriteLine($"{numbA} Второе число - {numbB} ");
+// }
+// else {Console.WriteLine ("Данное число, не трёхзначное ");}
+
+// Напиши программу, которая будет запрашивать у пользователя возраст.
+//   Если его возраст больше 18 - то выводим "Доступ разрешен"
+//   А если возраст меньше 18, то "Доступ запрещен"
+
+// Console.WriteLine("Введите возраст человека ");
+// int Age = Convert.ToInt32(Console.ReadLine());
+// if (Age > 18 )
+// {
+// Console.WriteLine("Доступ разрешен ");
+// }
+// else {Console.WriteLine("Доступ запрешен");}
+
+// 4) Напиши программу, которая будет проверять пароль пользователя при 
+// регистрации. Пользователь вбивает  пароль 2 раза. Если пароли совпадают, 
+// то выводим - "Регистрация успешна", если нет, то "Вбили разные пароли".
+
+// Console.WriteLine(" Введите пароль ");
+// int Pass = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(" Введите пароль повторно ");
+// int Pass2 = Convert.ToInt32(Console.ReadLine());
+// if (Pass == Pass2)
+// {
+// Console.WriteLine(" Регистрация успешна ");
+// }
+// else {Console.WriteLine(" Пароли не совпадают ");}
+
+// Напиши программу,  где вводим три числа, а потом 
+// выводим сумму только положительных чисел.
+
+// Console.WriteLine("Введите перове число");
+// Console.WriteLine("Введите второе число");
+// Console.WriteLine("Введите третье число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+// if (num1 > 0)
+// result = result + num1;
+// if (num2 > 0)
+// result = result + num2;
+// if (num3 > 0)
+// result = result + num3;
+// Console.WriteLine($"Сумма положительных чисел {result}");
