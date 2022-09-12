5346f7f32917fe4c6a669fcd1dae4101d361ca26
Mikheil007 <111079694+Mikheil007@users.noreply.github.com>
2022-09-12 10:31:20 +0300

    did my homework

---
+++ HomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1) Напиши программу, где вводим с клавиатуры три слова по очереди, 
+// а затем выводим их в обратной последовательности, каждое на отдельной строчке.
+
+String word1 , word2 , word3;
+Console.WriteLine("Enter the first word");
+ word1 = Console.ReadLine();
+Console.WriteLine("Enter the second word");
+ word2 = Console.ReadLine();
+Console.WriteLine("Enter the third word");
+ word3 = Console.ReadLine();
+Console.WriteLine(word3);
+Console.WriteLine(word2);
+Console.WriteLine(word1);
