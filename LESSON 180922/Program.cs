4a600e1cff6b40e75bef9810002bb7df17178476
Mikhail Abramishvili <mikhailabramishvili@MacBook-Air-Mikhail.local>
2022-09-18 15:27:09 +0300

    add lesson 180922

---
+++ LESSON 180922/Program.cs
@@ -0,0 +1,21 @@
+﻿// 2) Вводим с клавиатуры размер 3 сторон треугольника.
+//   И говорим какой он - обычный, равнобедренный или равносторонний
+
+Console.WriteLine("Сторону X сторону треугольника");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону Y треугольника");
+int Y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону Z треугольника");
+int Z = Convert.ToInt32(Console.ReadLine());
+if (X == Y & X == Z & Y == Z)
+{
+    Console.WriteLine("треугольник - равносторонний");
+}
+else if (X != Y & Z != Y & Z !=X)
+{
+    Console.WriteLine("треугольник - обычный");
+}
+else 
+{
+    Console.WriteLine("треугольник равнобедренный");
+}
